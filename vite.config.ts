e1371df12b15bed8aa154a1c2c2c90322460ae8e
Jerker Osatie <jerker.haggstrom.osatie@sundsvall.se>
2024-04-02 12:07:23 +0200

    removed build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
-import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), "");
+export default defineConfig(() => {
   return {
     plugins: [react()],
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: `assets/servicemodule-${env.VITE_APPLICATION}-[hash].js`,
-          assetFileNames: `assets/servicemodule-${env.VITE_APPLICATION}-[hash].[ext]`,
-        },
-      },
-    },
   };
 });
