1d145857201c8451e7b6864fbd305e6369de4d75
Jerker Osatie <jerker.haggstrom.osatie@sundsvall.se>
2024-04-03 09:10:47 +0200

    fixed some sizing bugs on small devices

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,15 +4,16 @@ import { useMediaQuery } from "usehooks-ts";
 
 interface InputProps extends ComponentPropsWithoutRef<"input"> {
   buttonDisabled?: boolean;
+  loading?: boolean;
 }
 export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-  const { className, buttonDisabled, ...rest } = props;
+  const { className, buttonDisabled, loading, ...rest } = props;
   const [hasFocus, setHasFocus] = useState<boolean>(false);
   const { theme } = useGui();
   const isSm = useMediaQuery(`screen and (max-width:${theme.screens.md})`);
 
   return (
-    <div className="h-48 sm:h-56 md:h-80 relative">
+    <div className={cx("h-48 sm:h-56 md:h-80 relative w-full", className)}>
       {!hasFocus && (
         <span className="absolute h-48 sm:h-56 md:h-80 flex items-center left-16 z-10">
           <Icon
@@ -26,8 +27,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
         ref={ref}
         {...rest}
         className={cx(
-          "font-medium h-48 sm:h-56 md:h-80 rounded-[0.8rem] bg-background-content opacity-85 focus:opacity-100 pl-[5.9rem] focus:pl-16 pr-[7.3rem] placeholder:text-black/50 text-dark-primary focus-visible:ring ring-ring ring-offset-0",
-          className
+          "font-medium  w-full h-48 sm:h-56 md:h-80 rounded-[0.8rem] bg-background-content opacity-85 focus:opacity-100 pl-[5.9rem] focus:pl-16 pr-[7.3rem] placeholder:text-black/50 text-dark-primary focus-visible:ring ring-ring ring-offset-0"
         )}
         onFocus={() => setHasFocus(true)}
         onBlur={() => setHasFocus(false)}
@@ -38,10 +38,13 @@ export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
           size={isSm ? "sm" : "md"}
           iconButton
           disabled={buttonDisabled}
-          className="opacity-45 hover:opacity-100 focus:opacity-100"
           color="bjornstigen"
         >
-          {buttonDisabled ? <Spinner /> : <Icon name="arrow-up" />}
+          {loading ? (
+            <Spinner size={isSm ? 3.2 : 4} />
+          ) : (
+            <Icon name="arrow-up" />
+          )}
         </Button>
       </div>
     </div>
