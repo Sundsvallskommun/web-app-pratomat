f8eb103dc7c9397c4ad2b29d16b947a6b565e9be
Emil Högberg <emil.hogberg@sundsvall.se>
2025-03-26 07:39:18 +0100

    fix: correctly import and use lang file

--- admin/src/components/background-color-picker/background-color-picker.tsx
+++ admin/src/components/background-color-picker/background-color-picker.tsx
@@ -1,17 +1,20 @@
 import { FormControl, FormLabel, Select } from '@sk-web-gui/react';
 import { useFormContext } from 'react-hook-form';
-import { backgroundColors } from '@public/locales/sv/assistants.json';
+import { useTranslation } from 'react-i18next';
 
 export const BackgroundColorPicker: React.FC<{ property: string; label: string }> = ({ property, label }) => {
   const { register } = useFormContext();
+  const { t } = useTranslation();
+
+  const backgroundColorKeys = ['vattjom', 'gronsta', 'bjornstigen', 'juniskar'];
 
   return (
     <FormControl>
       <FormLabel>{label}</FormLabel>
       <Select {...register(property)}>
-        {Object.entries(backgroundColors).map(([key, value]) => (
+        {backgroundColorKeys.map((key) => (
           <option key={key} value={key}>
-            {value}
+            {t(`assistants:backgroundColors.${key}`)}
           </option>
         ))}
       </Select>
